639a44525800456a95628fcf88c213fe096919ef
Lionel.Draghi <Lionel.Draghi@gmail.com>
2005-11-10 23:57:58 +0100

    Big Bang
    darcs-hash:7bca564f1943d8313166fc2e985b4f3bbb24e6e1

---
+++ Src/archicheck.adb
@@ -0,0 +1,31 @@
+with Ada.Text_IO;
+with Ada.Command_Line;
+
+with OpenToken.Text_Feeder.Text_IO;
+with Ada_Lexer;                      use  Ada_Lexer;
+
+procedure Archicheck is
+   -- Global text file for reading parse data
+   File : Ada.Text_IO.File_Type;
+begin
+   Ada.Text_IO.Open
+     (File => File,
+      Mode => Ada.Text_IO.In_File,
+      Name => Ada.Command_Line.Argument (1));
+
+   Ada.Text_IO.Set_Input (File);
+   Tokenizer.Input_Feeder := OpenToken.Text_Feeder.Text_IO.Create;
+
+   loop
+
+      Tokenizer.Find_Next (Analyzer);
+
+      Ada.Text_IO.Put_Line
+        ("Found " & Ada_Token'Image (Tokenizer.ID (Analyzer)) &
+         ' ' & Tokenizer.Lexeme (Analyzer));
+
+      exit when Tokenizer.ID (Analyzer) = End_of_File_T;
+
+   end loop;
+
+end Archicheck;
