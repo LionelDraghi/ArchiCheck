0c2fd701821e3ff218ff919f7b6b957a0bff29b8
Lionel.Draghi <Lionel.Draghi@gmail.com>
2009-10-17 19:10:43 +0200

    GPL3 License added to Ada sources files
    Ignore-this: 65a0467ae6b0866e78b04220fbab7968
    
    darcs-hash:7ccf0aee7370ce96b2d3929bcb409dd5bb9c03ba

--- Src/archicheck-main.ads
+++ Src/archicheck-main.ads
@@ -1,2 +1,11 @@
+-- -----------------------------------------------------------------------------
+-- ArchiCheck, the software architecture compliance verifier
+-- Copyright (C) 2005, 2006, 2009 - Lionel Draghi
+-- This program is free software;
+-- you can redistribute it and/or modify it under the terms of the GNU General
+-- Public License Versions 3, refer to the COPYING file.
+-- -----------------------------------------------------------------------------
+
+
 -- Procedure: Archicheck.Main specification
 private procedure Archicheck.Main;
--- Src/archicheck.ads
+++ Src/archicheck.ads
@@ -1,3 +1,12 @@
+-- -----------------------------------------------------------------------------
+-- ArchiCheck, the software architecture compliance verifier
+-- Copyright (C) 2005, 2006, 2009 - Lionel Draghi
+-- This program is free software;
+-- you can redistribute it and/or modify it under the terms of the GNU General
+-- Public License Versions 3, refer to the COPYING file.
+-- -----------------------------------------------------------------------------
+
+
 -- Package: Archicheck specification
 
 with Ada.Calendar;
