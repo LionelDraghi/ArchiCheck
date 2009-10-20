7593087c9d6f60778377d0f96be8339994aca2bb
Lionel.Draghi <Lionel.Draghi@gmail.com>
2009-10-20 23:25:32 +0200

    cleanup of temporary tests files and old OpenToken version
    Ignore-this: 74b2008e08fea639454d7f024386586a
    
    darcs-hash:0e5254ed3dcdebe70ac3d0ff102b3e80376e143d

--- OpenToken/Examples/Test/lookahead_test-parser.ads
+++
@@ -1,9 +0,0 @@
-with OpenToken.Token.Enumerated;
-with OpenToken.Token.Enumerated.Analyzer;
-
-package Lookahead_Test.Parser is
-   package Master_Example_Token is new Opentoken.Token.Enumerated (Example_Token_ID);
-   package Tokenizer is new Master_Example_Token.Analyzer;
-
-end Lookahead_Test.Parser;
-
