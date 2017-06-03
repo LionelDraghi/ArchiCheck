f43544cf3c006138e2b20e445d48a46cec191600
Lionel.Draghi <Lionel.Draghi@gmail.com>
2017-06-03 02:01:53 +0200

    OpenToken sources removed
    Ignore-this: fe46b7e331d68245001267edb1490dba
    
    darcs-hash:2e8b69a775c8bdb86fb96538ccf691585d585f8f

--- opentoken/Test/debug_parser.adb
+++
@@ -1,9 +0,0 @@
-with Verify_Aggregate.Parser; use Verify_Aggregate.Parser;
-procedure Debug_Parser
-is
-begin
-   Dump_Grammar;
-   Dump_Parse_Table;
-   Set_Trace (True);
-   Execute_Command ("set (2 =>;");
-end Debug_Parser;
